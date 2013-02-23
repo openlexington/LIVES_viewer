b380911aecbdca2c5f4679f059dd790df2c046fd
blakeshall <blake@blakeshall.com>
2013-02-23 17:26:52 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LIVESViewer::Application.config.secret_token = '7c15725f6784c72a5753ccd9b5b71cf610b0c7d4c6db4addecdddc899bac85f0b90b9a21ffc208205091ea056eb9b961a697de13be498d36b02a092cfe92119b'
